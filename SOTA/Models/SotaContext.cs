75e7a992796a88ffaac20ac2931be63035101c72
Даниил Хозя <I.O.Stalina@hotmail.com>
2020-02-12 18:08:39 +0300

    Занос в бд из wysiswyg Начало!!!

---
+++ SOTA/Models/SotaContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SOTA.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sota.Models
+{
+    public class SotaContext : DbContext
+    {
+        public DbSet<Zadanie> Zadanie { get; set; }
+      
+
+        public SotaContext(DbContextOptions<SotaContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
