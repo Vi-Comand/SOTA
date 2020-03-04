bfc42e8dd78894afa4190cbf182af1280d264784
Даниил Хозя <I.O.Stalina@hotmail.com>
2020-03-04 18:03:23 +0300

    111

--- SOTA/Controllers/UprSpecController.cs
+++ SOTA/Controllers/UprSpecController.cs
@@ -36,6 +36,35 @@ namespace SOTA.Controllers
             await db.SaveChangesAsync().ConfigureAwait(false);
             return Json("ok");
         }
+        public async Task<IActionResult> SaveOtveti(int tip, int idZadania, string[] arr, string[] arr1)
+        {
+            Zadanie EditZadanie = new Zadanie();
+            EditZadanie = db.Zadanie.Find(idZadania);
+            EditZadanie.Tip = tip;
+            try
+            {
+                List<Otvet> ListOtvets = new List<Otvet>();
+                for (int i=0;i<arr.Count();i++)
+                {
+                    Otvet AddOtvet = new Otvet();
+                    AddOtvet.IdZadan = idZadania;
+                    AddOtvet.Text = arr[i];
+                    if(tip ==2)
+                    {
+                        AddOtvet.Verno = Convert.ToInt32(arr1[i]); 
+                    }
+                    else
+                        AddOtvet.Verno = 1;
+                    ListOtvets.Add(AddOtvet);
+                }
+               
+                await db.Otvet.AddRangeAsync(ListOtvets).ConfigureAwait(false);
+                await db.SaveChangesAsync().ConfigureAwait(false);
+            }
+            catch(Exception ex)
+            { return Json(ex); }
+            return Json("ok");
+        }
         public IActionResult Privacy()
         {
             return View();
--- SOTA/Models/SotaDB.cs
+++ SOTA/Models/SotaDB.cs
@@ -12,4 +12,13 @@ namespace Sota.Models
         public int Variant { get; set; }
         public int Nomer { get; set; }
     }
+    public class Otvet
+    {
+        public int Id { get; set; }
+        public int IdZadan{ get; set; }
+        public int Verno { get; set; }
+        public float Param1 { get; set; }
+        public string Text { get; set; }
+       
+    }
 }
