ec4e030c39ee67c1986aa63cbb3580a77e81881c
Влад Кравченко <kravchenko.vi@live.com>
2020-05-01 19:43:00 +0300

    редизайн инпутов  форма созданий работ

---
+++ SOTA/Controllers/RabotaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SOTA.Controllers
+{
+    public class RabotaController : Controller
+    {
+        public IActionResult RabotaAdd()
+        {
+            return View();
+        }
+    }
+}
