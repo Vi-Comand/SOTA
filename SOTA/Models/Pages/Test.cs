278c4781adc6b67f125ea110dc77977e4fcf070a
Даниил Хозя <I.O.Stalina@hotmail.com>
2020-06-04 14:03:57 +0300

    11

---
+++ SOTA/Models/Pages/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOTA.Models
+{
+
+    public class Test
+    {
+        public List<ZadanNaProsmotr> Zadans { get; }
+        private readonly int _idSpec;
+        private readonly int _nVar;
+        private readonly SotaContext _db;
+     
+    }
+    public class OtvTest
+    {
+        int idOtv { get; set; }
+        int Text { get; set; }
+
+    }
+
+
+}
