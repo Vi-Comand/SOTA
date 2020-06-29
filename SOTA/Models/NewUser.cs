20f52a92397d028fb04ac963f971d65a8e7df5d9
kravchenko_v_e <kravcenko.vi@live.com>
2020-06-29 17:14:06 +0300

    загрузка юзеров

--- SOTA/Models/NewUser.cs
+++ SOTA/Models/NewUser.cs
@@ -20,7 +20,9 @@ namespace SOTA.Models
         public string O { get; set; }
         public string Klass { get; set; }
         public int NomKlass { get; set; }
-        public string Login { get; set; }
+        public string Name { get; set; }
+        public int Role { get; set; }
+        public DateTime DateReg { get; set; }
     }
 
     public class ReadExcel
@@ -54,7 +56,9 @@ namespace SOTA.Models
                     rowExcel.O = workSheet.Cells[i, 8].Value == null ? string.Empty : workSheet.Cells[i, 8].Value.ToString();
                     rowExcel.Klass = workSheet.Cells[i, 5].Value == null ? string.Empty : workSheet.Cells[i, 5].Value.ToString();
                     rowExcel.NomKlass = Convert.ToInt32(workSheet.Cells[i, 4].Value);
-                    //rowExcel.Login = workSheet.Cells[i, 9].Value.ToString();
+                    rowExcel.Name = workSheet.Cells[i, 9].Value == null ? string.Empty : workSheet.Cells[i, 9].Value.ToString();
+                    rowExcel.DateReg = Convert.ToDateTime("0001-01-01 00:00:00");
+                    rowExcel.Role = 0;
                     rowExcels.Add(rowExcel);
                 }
             }
@@ -71,6 +75,11 @@ namespace SOTA.Models
         List<Klass> ListKlass;
         List<Users> ListUserbd;
         List<Users> ListUser;
+        List<Users> load_rows;
+        List<Users> ListUserMO;
+        List<Users> ListUserOO;
+        List<Users> ListUserKlass;
+        List<Users> errore_rows;
         List<RowExcel> rowExcels;
         SotaContext db;
 
@@ -100,9 +109,13 @@ namespace SOTA.Models
             try
             {
                 db.Mo.AddRange(ListMO);
+
+
+
                 db.SaveChanges();
                 ListMObd.AddRange(ListMO);
             }
+
             finally
             {
                 IdMoVList();
@@ -115,6 +128,25 @@ namespace SOTA.Models
             foreach (Mo mos in ListMObd)
             {
                 rowExcels.Where(x => x.MO == mos.Name).AsParallel().ForAll(y => y.MO = mos.Id.ToString());
+            }
+
+            if (ListMO.Count != 0)
+            {
+                ListUserMO = new List<Users>();
+                for (int i = 0; i < ListMO.Count; i++)
+                {
+                    Users usersMo = new Users();
+                    usersMo.IdMo = ListMO[i].Id;
+                    usersMo.Role = 3;
+                    usersMo.Name = ListMO[i].Name + " администратор";
+                    usersMo.DateReg = Convert.ToDateTime("0001-01-01 00:00:00");
+
+                    ListUserMO.Add(usersMo);
+
+
+                }
+                db.Users.AddRange(ListUserMO);
+                db.SaveChanges();
             }
             CreateOO();
         }
@@ -143,6 +175,22 @@ namespace SOTA.Models
             {
                 rowExcels.Where(x => x.OO == oos.Name && x.MO == oos.IdMo.ToString()).AsParallel().ForAll(y => y.OO = oos.Id.ToString());
             }
+            if (ListOO.Count != 0)
+            {
+                ListUserOO = new List<Users>();
+                for (int i = 0; i < ListOO.Count; i++)
+                {
+                    Users usersOo = new Users();
+                    usersOo.IdMo = ListOO[i].IdMo;
+                    usersOo.IdOo = ListOO[i].Id;
+                    usersOo.Role = 2;
+                    usersOo.Name = "Директор " + ListOO[i].Id;
+                    usersOo.DateReg = Convert.ToDateTime("0001-01-01 00:00:00");
+                    ListUserOO.Add(usersOo);
+                }
+                db.Users.AddRange(ListUserOO);
+                db.SaveChanges();
+            }
             CreateKlass();
         }
 
@@ -170,8 +218,48 @@ namespace SOTA.Models
             {
                 rowExcels.Where(x => x.Klass == klass.Kod && x.NomKlass == klass.KlassNom && x.OO == klass.IdOo.ToString()).AsParallel().ForAll(y => y.Klass = klass.Id.ToString());
             }
-            CreateKlass();
+            if (ListKlass.Count != 0)
+            {
+                ListUserKlass = new List<Users>();
+                for (int i = 0; i < ListKlass.Count; i++)
+                {
+                    Users usersKl = new Users();
+                    usersKl.IdOo = ListKlass[i].IdOo;
+                    usersKl.IdKlass = ListKlass[i].Id;
+                    usersKl.Role = 1;
+                    usersKl.Name = "Класс " + ListKlass[i].Id;
+                    usersKl.DateReg = Convert.ToDateTime("0001-01-01 00:00:00");
+                    ListUserKlass.Add(usersKl);
+                }
+                db.Users.AddRange(ListUserKlass);
+                db.SaveChanges();
+            }
+            CreateUser();
         }
+
+        private void CreateUser()
+        {
+            ListUser = db.Users.ToList();
+            var load_rows1 = rowExcels.FindAll(w => ListUser.Find(x => w.Name == x.Name) == null);
+            var errore_rows1 = rowExcels.FindAll(w => (ListUser.Find(x => w.Name == x.Name) != null));
+
+            load_rows = load_rows1.Select(x => new Users { I = x.I, F = x.F, O = x.O, DateReg = x.DateReg, IdKlass = Convert.ToInt32(x.Klass), IdOo = Convert.ToInt32(x.OO), IdMo = Convert.ToInt32(x.MO), Role = Convert.ToInt32(x.Role), Name = x.Name }).ToList();
+
+            db.Users.AddRange(load_rows);
+            db.SaveChanges();
+
+            foreach (var row in load_rows1)
+            {
+
+            }
+
+
+        }
+
+
+
+
+
 
         private Random rnd;
         const string valid = "1234567890";
