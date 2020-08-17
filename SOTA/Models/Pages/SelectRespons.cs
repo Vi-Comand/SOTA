556e30e1232f35aa332986ba653d5cbf770ef56b
Влад Кравченко <kravchenko.vi@live.com>
2020-08-17 09:29:21 +0300

    11

---
+++ SOTA/Models/Pages/SelectRespons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace SOTA.Models
+{
+
+    public class SelectRespons
+    {
+        public List<Otvet> Otvets { get; }
+        private readonly string _idSpec;
+        private readonly string _nVar;
+        private readonly SotaContext _db;
+        public SelectRespons(SotaContext context)
+        {
+
+            _db = context;
+            Otvets = new List<Otvet>();
+
+        }
+
+    }
+
+}
