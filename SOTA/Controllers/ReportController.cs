47f36ba1ae35c48ba17495777987e45aef09448f
kravchenko_v_e <kravcenko.vi@live.com>
2020-12-30 15:10:34 +0300

    1

--- SOTA/Controllers/ReportController.cs
+++ SOTA/Controllers/ReportController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SOTA.Models;
+using SOTA.Models.Pages.Reports;
 using System;
 using System.Linq;
 using SOTA.Models.Pages.Reports;
@@ -18,7 +19,7 @@ namespace SOTA.Controllers
             db = context;
         }
         // [HttpGet]
-       
+
         [Authorize]
         [Route("Report/ReportsList/")]
         public IActionResult ReportsList()
@@ -39,15 +40,20 @@ namespace SOTA.Controllers
             var rabotaList = new ReportsRabotaList();
             if (user.Role == 2)
             {
-                 Tabl = new FormirRabotaTablList(user.IdOo, db);
-                rabotaList   = Tabl.GetSpisokRabotReports();
+                Tabl = new FormirRabotaTablList(user.IdOo, db);
+                rabotaList = Tabl.GetSpisokRabotReports();
             }
             if (user.Role == 3)
             {
                 Tabl = new FormirRabotaTablList(new Mo { Id = user.IdMo }, db);
                 rabotaList = Tabl.GetSpisokRabotReports();
             }
-            return View("RabotaList",rabotaList);
+            if (user.Role == 4)
+            {
+                Tabl = new FormirRabotaTablList(db);
+                rabotaList = Tabl.GetSpisokRabotReports();
+            }
+            return View("RabotaList", rabotaList);
         }
 
         [Route("Report/Download/{IdRabota?}/{Tip?}")]
@@ -56,12 +62,12 @@ namespace SOTA.Controllers
             string login = HttpContext.User.Identity.Name;
             Users user = db.Users.Where(p => p.Name == login).First();
             ViewBag.rl = user.Role;
-          
+
 
             ReportGenerator Report;
             if (user.Role == 2)
             {
-                 Report = new ReportTip1( db, IdRabota,new Oo {Id= user.IdOo });
+                Report = new ReportTip1(db, IdRabota, new Oo { Id = user.IdOo });
                 return RedirectToAction("File", "Download", new { path = Report.Create() });
             }
 
@@ -71,6 +77,20 @@ namespace SOTA.Controllers
                 return RedirectToAction("File", "Download", new { path = Report.Create() });
             }
 
+            if (user.Role == 4)
+            {
+                var mos = db.Mo.ToList();
+                for (int i = 0; i < mos.Count; i++)
+                {
+                    try
+                    {
+                        Report = new ReportTip1(db, IdRabota, new Mo { Id = mos[i].Id });
+                        RedirectToAction("File", "Download", new { path = Report.Create() });
+                    }
+                    catch
+                    { }
+                }
+            }
 
 
             return RedirectToAction("ReportsList");
